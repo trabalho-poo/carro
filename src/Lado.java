f5520135d96a7646b087a2568191f43d863d958a
Guilherme Mattos <45467253+guilhermemattos16@users.noreply.github.com>
2019-07-08 16:17:19 -0300

    Sincronizando Branchs

--- src/Lado.java
+++ src/Lado.java
@@ -1,4 +1,11 @@
-
+/**
+ * Classe que contem enumeracoes do tipo lado,
+ * para auxiliar na implementacao dos sensores.
+ * 
+ * @author Grupo (Cleisson diLauro, Franco Flores, Guilherme Mattos, Luciano
+ *         Alves, Natalia Lopes)
+ * @version 1.0(julho-2019)
+ */
 public enum Lado {
 	DIREITO, ESQUERDO;
 }
