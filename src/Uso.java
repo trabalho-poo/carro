86ed00555d4b4137c1f39cae6e58c7ac3778ce39
LucianoJunior-Lj <38591333+LucianoJunior-Lj@users.noreply.github.com>
2019-07-08 15:28:12 -0300

    Final

--- src/Uso.java
+++ src/Uso.java
@@ -1,127 +1,14 @@
-import lejos.hardware.Button;
-import java.util.*;
-import java.math.*;
 
 public class Uso {
 
 	public static void main(String[] args)
 	{
-		//10.7 graus do motor = 1 grau de 360
-		
-		float PROPORCAO = 10.6666666667f;
-		float[] distancias = new float[2];
 		VeiculoSmart carro = new VeiculoSmart(false, true, true, true);
-		float anguloDireita = 0, anguloEsquerda = 0, anguloRetorno = 0, distanciaRetorno = 0;
-		
-		carro.ligaSincronizacaoEsteiras();
-		carro.setVelocidadeEsteirasGrau(240);
-		carro.resetTacometro();
-		carro.encontraLinha();
-		distancias[0] = carro.getTacometroDireito();
-		//System.out.println(carro.getTacometroDireito() + "tacometro\n");
-		carro.resetTacometro();
-		if(carro.isPreto("esquerdo") && carro.isPreto("direito")) {
-			anguloDireita = 90;
-			anguloEsquerda = 90;
-		}
-		else if(carro.isPreto("esquerdo") && !carro.isPreto("direito")) {
-			carro.curvaEsquerdaCorrecao();
-			while(!carro.isPreto("direito"));
-			carro.stop();
-			anguloDireita = carro.getTacometroDireito() / PROPORCAO;
-			anguloEsquerda = 180 - anguloDireita;
-			carro.resetTacometro();
-		}
-		else if(!carro.isPreto("esquerdo") && carro.isPreto("direito")) {
-			carro.curvaDireitaCorrecao();
-			while(!carro.isPreto("esquerdo") && carro.isPreto("direito"));
-			carro.stop();
-			anguloEsquerda = carro.getTacometroEsquerdo() / PROPORCAO;
-			anguloDireita = 180 - anguloEsquerda;
-			carro.resetTacometro();
-		}
-		//System.out.println(anguloDireita + "angDireita" + anguloEsquerda +  "angEsquerda\n");		
-		carro.ligaSincronizacaoEsteiras();
-		carro.setEsteirasForward(1);
-		//carro.segueLinha();
-		carro.curvaEsquerda(4);
-		//carro.curvaEsquerdaCorrecao(2);
-		//carro.abreGarra();
-		carro.resetTacometro();
-		carro.ligaSincronizacaoEsteiras();
-		carro.segueLinhaAteBola();
-		distancias[1] = (carro.getTacometroDireito() + carro.getTacometroEsquerdo()) / 2;
-		//System.out.println(carro.getTacometroDireito() + "tacometro 2\n");
-		carro.resetTacometro();
-		//carro.ligaSincronizacaoEsteiras();
-		if(carro.isAchouBola()) {
-			distanciaRetorno = carro.calculaDistanciaRetorno(distancias, anguloEsquerda);
-			anguloRetorno = carro.calculaAnguloRetorno(distancias, distanciaRetorno);
-			anguloRetorno = (float) (Math.acos(anguloRetorno) * PROPORCAO);
-			carro.stop();
-			carro.voltaAoPontoDeOrigem(distanciaRetorno, anguloRetorno, "esquerdo");
-			
-		}else {
-			carro.setVelocidadeEsteirasGrau(100);
-			carro.setEsteirasForward(1);
-			carro.setVelocidadeEsteirasGrau(240);
-			carro.curvaEsquerdaCorrecao();
-			while(!carro.isPreto("esquerdo") || !carro.isPreto("direito"));
-			carro.stop();
-			carro.resetTacometro();
-			//carro.ligaSincronizacaoEsteiras();
-			carro.setVelocidadeEsteirasGrau(240);
-			carro.segueLinhaAteBola();
-			distancias[1] = ((carro.getTacometroDireito() + carro.getTacometroEsquerdo()) / 2) - distancias[1];
-			distanciaRetorno = carro.calculaDistanciaRetorno(distancias, anguloDireita);
-			anguloRetorno = carro.calculaAnguloRetorno(distancias, distanciaRetorno);
-			anguloRetorno = (float) (Math.acos(anguloRetorno) * PROPORCAO);
-			carro.stop();
-			carro.voltaAoPontoDeOrigem(distanciaRetorno, anguloRetorno, "direita");
-		}
-		//carro.largaBola();
-//		distanciaRetorno = (float) Math.sqrt((distancias[0] * distancias[0]) + (distancias[1] * distancias[1]) +
-//									2 * distancias[0] * distancias[1] * Math.cos(anguloDireita));
-//		
-//		anguloRetorno = (float) (((distancias[0] * distancias[0]) - (distancias[1] * distancias[1]) +
-//								 (distanciaRetorno * distanciaRetorno)) - (2 * distancias[0] * distancias[1])) ;
-//		}else {
-//			carro.curvaEsquerdaCorrecao(4);
-//			carro.curvaDireitaCorrecao();
-//			while((!carro.isPreto("esquerdo") && carro.isPreto("direito")) || (carro.isPreto("esquerdo") && !carro.isPreto("direito"))
-//					|| (!carro.isPreto("esquerdo") && !carro.isPreto("direito")));
-//			carro.stop();
-//			carro.ligaSincronizacaoEsteiras();
-//			carro.setVelocidadeEsteirasGrau(240);
-//			carro.segueLinhaAteBola();
-//			distancias.add(carro.getTacometroDireito());
-//			//System.out.println(carro.getTacometroDireito() + "tacometro3\n");
-//			if(carro.isAchouBola()) {
-//				carro.setVelocidadeEsteirasGrau(240);
-//				carro.setEsteirasForward(1);
-//				carro.fechaGarra();
-//				carro.resetTacometro();
-//				carro.ligaSincronizacaoEsteiras();
-//			}
-//		}
 
-// 		carro.resetTacometro();
-// 		carro.setVelocidadeEsteirasGrau(240);
-//		carro.curvaEsquerda(4);
-//		//carro.abreGarra();
-//		carro.segueLinha();
-//		carro.resetTacometro();
-//		carro.ligaSincronizacaoEsteiras();
-//		carro.segueLinhaAteBola();
-		/*distancias.add(carro.getTacometroDireito());
-		carro.resetTacometro();
-		if(carro.getDistancia() == 3) {
-			carro.segueLinhaAteBola();
-			distancias.add(carro.getTacometroDireito());
-			carro.resetTacometro();
-		}
-		carro.seguraBola();
-		carro.voltaAoPontoDeOrigem();*/
+		carro.encontraLinha();
+		carro.segueLinhaAteBola();
+		carro.voltaAoPontoDeOrigem();
+		
 		carro.ev3.beep5();
 		carro.ev3.corLed(3);
 	}
--- src/VeiculoSmart.java
+++ src/VeiculoSmart.java
@@ -4,8 +4,11 @@
  * @version 1.0(julho-2019)
  */
 public class VeiculoSmart extends Veiculo{
-	
-	private boolean achouBola = false;
+	// 10.6666666667 graus do motor = 1 grau de 360
+	private float PROPORCAO = 10.6666666667f;
+	private float[] distancias = new float[2];
+	private float anguloDireita = 0, anguloEsquerda = 0, anguloRetorno = 0, distanciaRetorno = 0;
+	private boolean achouBolaNaEsquerda = false, achouBola = false;
 	/**
 	 * construtor de veiculo definindo quais sensores serao ativados
 	 * @param toque : boolean ativa sensor de toque
@@ -16,7 +19,7 @@ public class VeiculoSmart extends Veiculo{
 	public VeiculoSmart(boolean toque, boolean colorDir, boolean colorEsq, boolean infravermelho) {
 		super(toque, colorDir, colorEsq, infravermelho);
 	}
-	
+
 	/**
 	 * Metodo para resertar o tacometro dos motores esquerdo e direito
 	 */
@@ -24,7 +27,7 @@ public class VeiculoSmart extends Veiculo{
 		this.dir.resetTacometro();
 		this.esq.resetTacometro();
 	}
-	
+
 	/**
 	 * Metodo para pegar quantos graus o motor direito girou
 	 * @return this.dir.getTacometro() : quantidade de graus que o motor direito girou
@@ -32,7 +35,7 @@ public class VeiculoSmart extends Veiculo{
 	public float getTacometroDireito() {
 		return this.dir.getTacometro();
 	}
-	
+
 	/**
 	 * Metodo para pegar o quantos graus o motor esquerdo girou
 	 * @return this.esq.getTacometro() : quantidade de graus que o motor direito girou
@@ -40,200 +43,267 @@ public class VeiculoSmart extends Veiculo{
 	public float getTacometroEsquerdo() {
 		return this.esq.getTacometro();
 	}
-	
+
 	/**
-	 * Metodo para fazer o carro andar para frente ate um dos sensores 
-	 * encontrar a cor preta
+	 * Metodo para o carro andar para frente ate encontrar a linha e registrar a distancia percorrida
 	 */
 	public void encontraLinha() {
 		if(this.corDirAtivo && this.corEsqAtivo)
 		{
+			this.setVelocidadeEsteirasGrau(240);
 			this.setEsteirasForward();
 			while(!this.isPreto("esquerdo") && !this.isPreto("direito"));
 			this.stop();
+			distancias[0] = this.getTacometroDireito();
+			this.resetTacometro();
 		}
 	}
-	
+
 	/**
-	 * 
+	 * Metodo para calcular a angulacao do carro com relacao a linha 
+	 * e o carro virar a esquerda para dentro da linha
+	 */
+	public void entraNaLinha() {
+
+		//Se os sensores da esquerda e direita chegarem ao mesmo tempo na linha,
+		//o carro fica com 90 graus de angulacao para o lado direito e esquerdo da linha
+		if(this.isPreto("esquerdo") && this.isPreto("direito")) {
+			anguloDireita = 90;
+			anguloEsquerda = 90;
+		}
+		//Se o sensor esquerdo chegar primeiro na linha, o carro curva para esquerda
+		//ate o sensor direito chegar a linha, e converte a rotacao do motor direito
+		//para a angulacao do carro em relacao ao lado direito da linha e calcula 
+		//a angulacao do carro em relacao ao lado esquerdo da linha
+		else if(this.isPreto("esquerdo") && !this.isPreto("direito")) {
+			this.curvaEsquerda();
+			while(!this.isPreto("direito"));
+			this.stop();
+			anguloDireita = this.getTacometroDireito() / PROPORCAO;
+			anguloEsquerda = 180 - anguloDireita;
+			this.resetTacometro();
+		}
+		//Se o sensor direito chegar primeiro na linha, o carro curva para direita
+		//ate o sensor esquerdo chegar a linha, e converte a rotacao do motor esquerdo
+		//para a angulacao do carro em relacao ao lado esquerdo da linha e calcula
+		//a angulacao do carro em relacao ao lado direito da linha
+		else if(!this.isPreto("esquerdo") && this.isPreto("direito")) {
+			this.curvaDireita();
+			while(!this.isPreto("esquerdo") && this.isPreto("direito"));
+			this.stop();
+			anguloEsquerda = this.getTacometroEsquerdo() / PROPORCAO;
+			anguloDireita = 180 - anguloEsquerda;
+			this.resetTacometro();
+		}
+		//Vira o carro para esquerda
+		this.setEsteirasForward(1);
+		this.curvaEsquerdaNoEixo(4);
+		this.resetTacometro();
+	}
+
+	/**
+	 * Metodo para controlar o carro dentro da linha preta ate chegar
+	 * no final ou encontrar a bola, caso encontre a bola, pega a mesma
 	 */
 	public void segueLinha() {
 		if(this.corDirAtivo && this.corEsqAtivo)
 		{
-			this.setEsteirasForward();
-			while(this.isPreto("esquerdo") && this.isPreto("direito"));
-			this.ev3.corLed(7);
-			this.ev3.beep2();
-			this.stop();
-		}
-	}
-	
-	/**
-	 * Metodo para controlar o carro dentro da linha preta ate 
-	 * ele chegar ao final da linha ou encontrar a bola,  
-	 */
-	public void segueLinhaAteBola() {
-		if(this.corDirAtivo && this.corEsqAtivo && this.infravermIsAtivo)
-		{
 			this.ligaSincronizacaoEsteiras();
 			this.setEsteirasForward();
-			//carro fica andando enquanto os sensores de cor ver preto 
-			//e o sensor de infravermelho tem a distancia do chao
+			//Carro segue a linha enquanto nao acha bola e um dos sensores nao sai da linha
 			while(this.isPreto("esquerdo") && this.isPreto("direito") && (this.getDistancia() > 6));
-			
-			//se o sensor de cor da esquerda sair da linha e o 
-			//sensor da direita ficar na linha,
-			//carro virar para direita ate os dois senhores ficarem dentro da linha,
+
+			//se o carro sai da linha pela esquerda, ele curva para direita ate voltar para linha
 			//e chama o metodo segueLinhaAteBola recursivamente
 			if(!this.isPreto("esquerdo") && this.isPreto("direito")){
 				this.curvaDireita();
 				while(!this.isPreto("esquerdo") && this.isPreto("direito"));
-				//this.stop();
-				this.segueLinhaAteBola();
+				this.segueLinha();
 			}
-			//se nao se o sensor de cor da direita sair da linha e o 
-			//sensor da esquerda ficar na linha,
-			//carro virar para esquerda ate os dois senhores ficarem dentro da linha,
+			//se o carro sai da linha pela direita, ele curva para esquerda ate voltar para linha
 			//e chama o metodo segueLinhaAteBola recursivamente
 			else if(this.isPreto("esquerdo") && !this.isPreto("direito")) {
 				this.curvaEsquerda();
 				while(this.isPreto("esquerdo") && !this.isPreto("direito"));
-				//this.stop();
-				this.segueLinhaAteBola();
+				this.segueLinha();
 			}
-			//else if(!this.isPreto("esquerdo") && !this.isPreto("direito"))
-//			this.stop();
-			
-			//se sensor de infravermelho detectar a bola chama o metodo pegaBolaNaLinha
-			else if(this.getDistancia() < 6 && !this.isAchouBola()) {
+			//Se o carro encontra a bola, chama o metodo para pegar a mesma
+			else if(this.getDistancia() < 6) {
 				this.stop();
 				this.achouBola = true;
 				this.pegaBolaNaLinha();
 			}
 			this.stop();
-		
 		}
 	}
-	
+
 	/**
-	 * Metodo para fazer o carro pegar a bola
+	 * Metodo para calcular a distancia do carro percorrida em cima da linha e verificar se a bola esta 
+	 * de um lado da linha, caso nao esteja carro vira 180 graus e vai para a outra ponta da linha
 	 */
-	public void seguraBola() {
-		this.abreGarra();
-		this.setVelocidadeEsteirasGrau(72);
-		this.setEsteirasForward(1);
-		this.fechaGarra();
+	public void segueLinhaAteBola() {
+		if(this.corDirAtivo && this.corEsqAtivo && this.infravermIsAtivo)
+		{
+			//chama o metodo para o carro entrar na linha
+			this.entraNaLinha();
+			//chama o metodo para o carro seguir ate o final da linha
+			this.segueLinha();
+			//calcula a distancia percorrida na linha 
+			this.distancias[1] = (this.getTacometroDireito() + this.getTacometroEsquerdo()) / 2;
+			this.resetTacometro();
+
+			//se o carro achou a bola na primeira chamada de segue linha, 
+			//significa que a bola estava no lado esquerdo da linha
+			if(this.achouBola) {
+				this.achouBolaNaEsquerda = true;
+			}
+
+			//se nao carro da meia volta e procura a bola no lado direito
+			else {
+				this.setVelocidadeEsteirasGrau(100);
+				this.setEsteirasForward(1);
+				this.curvaEsquerdaNoEixo();
+				while(!this.isPreto("esquerdo") || !this.isPreto("direito"));
+				this.stop();
+				this.resetTacometro();
+				this.segueLinha();
+
+				//calcula a distancia percorrida de uma ponta ate a outra da linha e diminue pela
+				//distancia que o carro tinha percorrido da chegada na linha ate o final do lado esquerdo
+				this.distancias[1] = ((this.getTacometroDireito() + this.getTacometroEsquerdo()) / 2) - this.distancias[1];
+				this.resetTacometro();
+				this.achouBolaNaEsquerda = false;
+			}
+			this.stop();
+			//this.ev3.esperaSegundos(3);
+		}
 	}
-	
+
 	/**
-	 * Metodo para fazer o carro soltar a bola
+	 * Metodo para o carro soltar a bola
 	 */
 	public void largaBola() {
 		this.abreGarra();		
 	}
-	
+
 	/**
-	 * Metodo para dar comandos ao carro para pegar a bola
+	 * Metodo para o carro pegar a bola
 	 */
 	public void pegaBolaNaLinha() {
 		this.ligaSincronizacaoEsteiras();
 		this.abreGarra();
 		this.ev3.esperaSegundos(1);
-		this.setVelocidadeEsteirasGrau(200);
+		this.setVelocidadeEsteirasGrau(240);
 		this.setEsteirasForward(1);
 		this.fechaGarra();
 	}
-	
+
 	/**
-	 * Metodo para
+	 * Metodo para o carro calcular o quanto ele tem que virar, para qual lado tem que virar
+	 * e o quanto ele tem que andar ate voltar ao ponto de origem e 
+	 * faz o carro voltar ao ponto de origem
 	 */
-	public void voltaAoPontoDeOrigem(float distanciaRetorno, float anguloRetorno, String lado) {	
+	public void voltaAoPontoDeOrigem() {	
 		this.setVelocidadeEsteirasGrau(240);
-		if (lado == "esquerda") {
-			this.ev3.beep1();
-			this.curvaEsquerda((int)(anguloRetorno / 240));
-			this.ev3.beep1();
-		}else if(lado == "direita") {
-			this.ev3.beep1();
-			this.curvaDireita((int)(anguloRetorno / 240));
-			this.ev3.beep1();
+		//Se o carro achou bola na esquerda, ele tem que virar pra esquerda para voltar ao ponto de origem
+		if(this.achouBolaNaEsquerda) {
+			this.calculaDistanciaRetorno(anguloEsquerda);
+			this.calculaAnguloRetorno();
+			this.anguloRetorno = 180 - (float) (Math.acos(anguloRetorno));
+			this.anguloRetorno = this.anguloRetorno * PROPORCAO;
+			//this.ev3.beep1();
+			this.curvaEsquerda((int)(anguloRetorno / 240.0 + 1));
+			//this.ev3.beep1();
+			this.ligaSincronizacaoEsteiras();
+			this.setEsteirasForward((int) (distanciaRetorno / 240.0 + 1));
+			this.stop();
 		}
-		this.ligaSincronizacaoEsteiras();
-		this.setVelocidadeEsteirasGrau(240);
-		this.setEsteirasForward((int) (distanciaRetorno / 240));
-		this.stop();
+		//Se o carro achou bola na esquerda, ele tem que virar pra direita para voltar ao ponto de origem
+		else {
+			this.calculaDistanciaRetorno(anguloDireita);
+			this.calculaAnguloRetorno();
+			this.anguloRetorno = 180 - (float) (Math.acos(anguloRetorno));
+			this.anguloRetorno = this.anguloRetorno * PROPORCAO;
+			//this.ev3.beep1();
+			this.curvaDireita((int)(anguloRetorno / 240.0 + 1));
+			//this.ev3.beep1();
+			this.ligaSincronizacaoEsteiras();
+			this.setEsteirasForward((int) (distanciaRetorno / 240.0 + 1));
+			this.stop();
+		}
 	}
-	
+
 	/**
-	 * Metodo para carro virar para a direita
-	 * com uma esteira para frente e outra para tras
+	 * Metodo para carro virar para a direita no proprio eixo
 	 */
-	public void curvaDireitaCorrecao()
+	public void curvaDireitaNoEixo()
 	{
 		this.desligaSincronizacaoEsteiras();
 		this.esq.ligaFrente();
 		this.dir.ligaTras();
+		this.ligaSincronizacaoEsteiras();
 	}
-	
+
 	/**
-	 * Metodo para veiculo virar para a esquerda
-	 * com uma esteira para frente e outra para tras
+	 * Metodo para veiculo virar para a esquerda no proprio eixo
 	 */
-	public void curvaEsquerdaCorrecao()
+	public void curvaEsquerdaNoEixo()
 	{
 		this.desligaSincronizacaoEsteiras();
 		this.dir.ligaFrente();
 		this.esq.ligaTras();
-		
+		this.ligaSincronizacaoEsteiras();
+
 	}
-	
+
 	/**
-	 * Metodo para veiculo virar para esquerda por um determinado tempo
-	 * com uma esteira para frente e a outra para tras 
+	 * Metodo para veiculo virar para a esquerda no proprio eixo durante um determinado tempo
 	 * @param tempo
 	 */
-	public void curvaEsquerdaCorrecao(int tempo)
+	public void curvaEsquerdaNoEixo(int tempo)
 	{
 		this.desligaSincronizacaoEsteiras();
-		this.esq.ligaTras(tempo);
-		this.dir.ligaFrente(tempo);
-		
+		this.esq.ligaTras();
+		this.dir.ligaFrente();
+		this.ev3.esperaSegundos(tempo / 2);
+		this.ligaSincronizacaoEsteiras();
+
 	}
-	public void curvaDireitaCorrecao(int tempo)
+
+	/**
+	 * Metodo para veiculo virar para a direita no proprio eixo durante um determinado tempo
+	 * @param tempo
+	 */
+	public void curvaDireitaNoEixo(int tempo)
 	{
 		this.desligaSincronizacaoEsteiras();
-		this.esq.ligaTras(tempo);
-		this.dir.ligaFrente(tempo);
-		
+		this.esq.ligaTras();
+		this.dir.ligaFrente();
+		this.ev3.esperaSegundos(tempo);
+		this.ligaSincronizacaoEsteiras();
+
 	}
-	
+
 	/**
 	 * Metodo para calcular a distancia que o carro tera que percorrer ate o ponto de origem
 	 * @param distancias
 	 * @param angulo
 	 * @return distancia de retorno
 	 */
-	public float calculaDistanciaRetorno(float[] distancias, float angulo) {
-		return (float) (Math.sqrt((distancias[0] * distancias[0]) + (distancias[1] * distancias[1]) +
-								  (2 * distancias[0] * distancias[1] * Math.cos(angulo))));
+	public void calculaDistanciaRetorno(float angulo) {
+		this.distanciaRetorno = (float) (Math.sqrt((this.distancias[0] * this.distancias[0]) + (this.distancias[1] * this.distancias[1])
+				- (2 * this.distancias[0] * this.distancias[1] * Math.cos(angulo))));
 	}
-	
+
 	/**
 	 * Metodo para calcular o angulo que o carro tera que virar para voltar ao ponto de origem
 	 * @param distancias
 	 * @param distanciaRetorno
 	 * @return angulo de retorno
 	 */
-	public float calculaAnguloRetorno(float[] distancias, float distanciaRetorno) {
-		return (float) ((distancias[0] * distancias[0] - distancias[1] * distancias[1] -
-				         distanciaRetorno * distanciaRetorno) / (- (2 * distancias[0] * distancias[1]))) ;
-	}
-	
-	/**
-	 * Metodo para verificar se a bola foi encontrada
-	 * @return achouBola : boolean
-	 */
-	public boolean isAchouBola() {
-		return this.achouBola;
+	public void calculaAnguloRetorno() {
+		this.anguloRetorno = (float) ((this.distanciaRetorno * this.distanciaRetorno - this.distancias[0] * this.distancias[0] + 
+				this.distancias[1] * this.distancias[1]) / (2 * this.distanciaRetorno * this.distancias[1])) ;
+
 	}
 }
